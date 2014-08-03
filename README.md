4d64b1f069b8e2015b183df08c023f5d43ba1414
unknown <wehu@SHALWEHU2.amd.com>
2014-08-03 13:42:16 +0800

    refine 	modified:   README.md 	modified:   src/AST.hs 	modified:   src/Main.hs 	modified:   src/Module.hs 	modified:   src/Parser.hs 	modified:   src/Resolver.hs 	modified:   src/Transformer.hs 	modified:   src/Type.hs 	modified:   src/TypeInfer.hs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## HW
+
+HW is a language for hardware simulation. It is still under development.
+
+### Example
+
+	foo = 1 |> (\a -> (3 + a))
+
+	main = let i = liftS2 (\s1 s2 -> s1 + s2 + foo)
+	           <| (foldS (\acc a -> acc + 1) 1 clk)
+	           <| (liftS (\a -> 1) clk)
+	     in i
--- src/Transformer.hs
+++ src/Transformer.hs
@@ -1,3 +1,19 @@
+{-
+   Copyright 2014 huwei04@hotmail.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
 module Transformer (
   transform2hs,
   nullSignalState
@@ -160,7 +176,7 @@ transform2hs clk m = do
    in do
         (_, ts) <- get
         if (TI.ftv $ Set.toList ts) /= Set.empty
-        then throwError $ "`main' returns the type including type variables " ++ (show $ Set.toList ts)
+        then throwError $ "cannot resolve type variables in " ++ (show $ Set.toList ts)
         else let r1 = case (Set.toList ts) of
                        [] -> "\n\n"
                        [t] -> "data SignalValue = T" ++ (uniqTC t) ++ " " ++ (type2hs t) ++ "\n\n"
